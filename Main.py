0d7a58637af4a2a0be1e3353d3a8b0931428ba56
hummingjay <jays7mill@gmail.com>
2024-04-22 08:09:58 +0300

    Restructure

--- Main.py
+++ Main.py
@@ -3,16 +3,15 @@ from flet import (
     Page,
     colors
 )
- 
+
+def main(page: Page):
+
+    page.title = "Flet Trello clone"
+    page.padding = 0
+    page.bgcolor = colors.BLUE_GREY_200
+    app = TrelloApp(page)
+    page.add(app)
+    page.update()
+
 if __name__ == "__main__":
- 
-    def main(page: Page):
- 
-        page.title = "Flet Trello clone"
-        page.padding = 0
-        page.bgcolor = colors.BLUE_GREY_200
-        app = TrelloApp(page)
-        page.add(app)
-        page.update()
- 
     flet.app(target=main, view=flet.WEB_BROWSER)
