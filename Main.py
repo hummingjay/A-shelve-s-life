fc7307606574a46a74c4a065f5752a60334d6993
Hummingjay <jays7mill@gmail.com>
2024-04-24 06:46:31 +0300

    Phase one from trolli

--- Main.py
+++ Main.py
@@ -1,17 +1,57 @@
 import flet
 from flet import (
     Page,
-    colors
+    colors,
+    UserControl,
+    Column,
+    Container,
+    Row,
+    icons,
+    Icon,
+    margin, 
+    Text,
+    AppBar,
+    PopupMenuButton,
+    PopupMenuItem,
 )
 
-def main(page: Page):
+class Shelf_life(UserControl):
+    def __init__(self, page: Page):
+        super().__init__()
+        self.page = page
+        self.appbar_items = [
+            PopupMenuItem(icon=icons.PERSON_OUTLINED, text="Login"),
+            PopupMenuItem(), # a divider
+            PopupMenuItem(icon=icons.SETTINGS_OUTLINED)
+        ]
+        self.appbar = AppBar(
+            leading=Icon(icons.HOME)
+            leading_width=100,
+            title=Text("A Shelve's Life"),
+            center_title=False,
+            toolbar_height=75,
+            bgcolor=colors.LIGHT_BLUE_ACCENT_700,
+            actions=[
+                Container(
+                    content=PopupMenuButton(
+                        items=self.appbar_items
+                    ),
+                    margin=margin.only(left=50, right=25)
+                )
+            ],
+        )
+        self.page.appbar = self.appbar
+        self.page.update()
 
-    page.title = "Flet Trello clone"
+
+
+def main(page: Page):
+    page.title = "A Shelve's Life"
     page.padding = 0
     page.bgcolor = colors.BLUE_GREY_200
-    app = TrelloApp(page)
+    app = Shelf_life(page)
     page.add(app)
     page.update()
 
 if __name__ == "__main__":
-    flet.app(target=main, view=flet.WEB_BROWSER)
+    flet.app(target=main)
